808ba4c1e3453741f7e7b232f4cc2375cc7908e3
José Pinto <jose.ferropinto@epfl.ch>
2018-04-09 19:35:13 +0200

    UPDATE : EnigmeActivity is used to show enigme

--- app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/EnigmeActivity.kt
+++ app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/EnigmeActivity.kt
@@ -6,20 +6,19 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 
-
-
 class EnigmeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_enigme)
-        val img = findViewById<View>(R.id.imageView) as ImageView
+        setContentView(R.layout.activity_enigme2)
+        val img = findViewById<View>(R.id.imageView2) as ImageView
+
         img.isClickable = true
         img.setOnClickListener(View.OnClickListener {
             Toast.makeText(this@EnigmeActivity,
                     "The favorite list would appear on clicking this icon",
                     Toast.LENGTH_LONG).show()
         })
-        img.setImageResource(R.drawable.carte1)
+        img.setImageResource(R.drawable.enigme1)
     }
 }
