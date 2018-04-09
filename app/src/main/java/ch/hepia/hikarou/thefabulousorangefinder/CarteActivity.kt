6fd743fe1b228fe5ea4a7d3560130d8192a7c5d4
José Pinto <jose.ferropinto@epfl.ch>
2018-04-09 19:42:03 +0200

    UPDATE : CarteActivity goes to Enigme activity on click

--- app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/CarteActivity.kt
+++ app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/CarteActivity.kt
@@ -4,21 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
-
+import android.content.Intent
 
 
 class CarteActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enigme)
         val img = findViewById<View>(R.id.imageView) as ImageView
         img.isClickable = true
         img.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this@CarteActivity,
-                    "The favorite list would appear on clicking this icon",
-                    Toast.LENGTH_LONG).show()
+            val intent = Intent(this@CarteActivity, EnigmeActivity::class.java)
+            startActivity(intent)
         })
         img.setImageResource(R.drawable.carte1)
     }
