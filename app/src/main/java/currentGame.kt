2c308e0d8868ef8ce6c48f4156d74518cffef49e
José Pinto <jose.ferropinto@epfl.ch>
2018-04-10 11:24:10 +0200

    ADD: currentGame stores the current Game

--- app/src/main/java/currentGame.kt
+++ app/src/main/java/currentGame.kt
@@ -1,3 +1,17 @@
-object currentGame {
+import ch.hepia.hikarou.thefabulousorangefinder.Game
 
+object currentGame {
+    private lateinit var curGame : Game
+
+    fun setCurGame(game: Game) {
+        curGame = game
+    }
+
+    fun getCurStep(): Int {
+        return curGame.currentStep
+    }
+
+    fun nextTag(): String {
+        return curGame.gameTags[curGame.currentStep]
+    }
 }
