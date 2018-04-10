4bcafad06570c37e41784fa2319f2e9b2a9576f6
José Pinto <jose.ferropinto@epfl.ch>
2018-04-10 19:09:30 +0200

    UDPATE : add test tags and real tags

--- app/src/main/java/DifferentGames.kt
+++ app/src/main/java/DifferentGames.kt
@@ -1,3 +1,12 @@
 object DifferentGames {
+    // Test tag (golden) :
+    //  - E2717630000007E0 (golden)
+    //  - 88C1992C000007E0 (silver)
+    // Real tags :
+    //  - 04ECD3DAAB4A80
+    //  - 04AED3DAAB4A80
+    //  - 0457D2DAAB4A81
+    //  - 043CD3DAAB4A80
+    //  - 04B5D2DAAB4A80
     val firstGame = arrayOf("04ECD3DAAB4A80", "04AED3DAAB4A80", "0457D2DAAB4A81", "043CD3DAAB4A80", "04B5D2DAAB4A80")
 }
