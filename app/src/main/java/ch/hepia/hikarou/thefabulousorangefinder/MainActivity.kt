e9132decb219f82c9724ee7821512de45d22ea80
José Pinto <jose.ferropinto@epfl.ch>
2018-04-10 20:40:51 +0200

    CLEANUP : Adding comments for readability

--- app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/MainActivity.kt
+++ app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/MainActivity.kt
@@ -1,6 +1,5 @@
 package ch.hepia.hikarou.thefabulousorangefinder
 
-
 import CurrentGame
 import android.content.Intent
 import android.nfc.NfcAdapter
@@ -8,7 +7,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -16,6 +14,9 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Will be called everytime the activity is called
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,28 +25,32 @@ class MainActivity : AppCompatActivity() {
         val txt = findViewById<TextView>(R.id.textView)
         txt.typeface = ResourcesCompat.getFont(this.applicationContext, R.font.font)
 
+        // New game button
         val btNewGame = findViewById<Button>(R.id.button)
         btNewGame?.setOnClickListener {
             CurrentGame.createNewGame("Toto", DifferentGames.firstGame, this@MainActivity)
             startActivity(Intent(this@MainActivity, CarteActivity::class.java))
         }
 
+        // continue button
         val btContinue = findViewById<Button>(R.id.button2)
         btContinue?.setOnClickListener {
-            // get the game state
             CurrentGame.init(this@MainActivity)
-            startActivity (Intent(this@MainActivity, CarteActivity::class.java))
+            startActivity(Intent(this@MainActivity, CarteActivity::class.java))
         }
 
+        // NFC tag handling
         if (intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
             CurrentGame.processIntent(intent, this@MainActivity)
 
-            startActivity(Intent(this@MainActivity, CarteActivity::class.java))
             startActivity(Intent(this@MainActivity, EnigmeActivity::class.java))
         }
     }
 
     private var exit = false
+    /**
+     * Handles the back button press, asks confirmation by repressing the button in the next 3 seconds
+     */
     override fun onBackPressed() {
         if (exit) {
             finish()
