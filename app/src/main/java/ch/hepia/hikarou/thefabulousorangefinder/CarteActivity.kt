a11a070694ac600370c899a024510e7bf4fa22c0
José Pinto <jose.ferropinto@epfl.ch>
2018-04-10 12:44:11 +0200

    ADD: Carte gets the tags

--- app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/CarteActivity.kt
+++ app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/CarteActivity.kt
@@ -1,15 +1,14 @@
 package ch.hepia.hikarou.thefabulousorangefinder
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import android.content.Intent
-import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.nfc.Tag
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
-import kotlin.experimental.and
+import currentGame
 
 
 class CarteActivity : AppCompatActivity() {
@@ -18,7 +17,7 @@ class CarteActivity : AppCompatActivity() {
         setContentView(R.layout.activity_enigme)
         val img = findViewById<View>(R.id.imageView) as ImageView
         img.isClickable = true
-        img.setOnClickListener(View.OnClickListener {
+        img.setOnClickListener({
             val intent = Intent(this@CarteActivity, EnigmeActivity::class.java)
             startActivity(intent)
         })
@@ -33,5 +32,51 @@ class CarteActivity : AppCompatActivity() {
         }
 
         img.setImageResource(carte)
+
+        if (intent != null) {
+            processIntent(intent)
+        }
+    }
+
+
+    private fun processIntent(checkIntent: Intent) {
+        // Check if intent has the action of a discovered NFC tag
+        // with NDEF formatted contents
+        if (checkIntent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
+
+            val tag: Tag = checkIntent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+
+            if (byteArrayToHexString(tag.id) == currentGame.nextTag()) {
+                Toast.makeText(this@CarteActivity,
+                        "Bien joué! Tu as trouvé le bon tag pour cette étape !",
+                        Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this@CarteActivity,
+                        "Malheuresement, pas le bon tag !",
+                        Toast.LENGTH_LONG).show()
+            }
+            //TODO Erase this
+            Toast.makeText(this@CarteActivity,
+                    "ID : ${byteArrayToHexString(tag.id)}",
+                    Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun byteArrayToHexString(inarray: ByteArray): String {
+        var i: Int
+        var j = 0
+        var `in`: Int
+        val hex = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
+        var out = ""
+
+        while (j < inarray.size) {
+            `in` = inarray[j].toInt() and 0xff
+            i = `in` shr 4 and 0x0f
+            out += hex[i]
+            i = `in` and 0x0f
+            out += hex[i]
+            ++j
+        }
+        return out
     }
 }
