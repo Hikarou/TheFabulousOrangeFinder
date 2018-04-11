81f3d4a7a4aea74c3d65155977ed57339aa7b49b
José Pinto <jose.ferropinto@epfl.ch>
2018-04-11 15:09:12 +0200

    UPDATE : Check the NFC support and activation

--- app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/MainActivity.kt
+++ app/src/main/java/ch/hepia/hikarou/thefabulousorangefinder/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -20,6 +21,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // NFC support
+        val nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "This device does not support NFC",
+                    Toast.LENGTH_SHORT).show()
+            finish()
+        }
 
         // Layout
         val txt = findViewById<TextView>(R.id.textView)
@@ -39,8 +48,19 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity, CarteActivity::class.java))
         }
 
+        if (!nfcAdapter.isEnabled) {
+            Toast.makeText(this, "You need to activate NFC before you can play",
+                    Toast.LENGTH_SHORT).show()
+            val buttons = arrayOf(btNewGame, btContinue)
+            buttons.forEach { it.visibility = View.INVISIBLE }
+            btNewGame.visibility = View.INVISIBLE
+            btContinue.visibility = View.INVISIBLE
+            checkNfcBack(nfcAdapter, buttons)
+        }
+
+
         // NFC tag handling
-        if (intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
+        if (CurrentGame.isInited() && intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
             CurrentGame.processIntent(intent, this@MainActivity)
 
             startActivity(Intent(this@MainActivity, EnigmeActivity::class.java))
@@ -61,5 +81,15 @@ class MainActivity : AppCompatActivity() {
             Handler().postDelayed({ exit = false }, 3 * 1000)
         }
 
+    }
+
+    /**
+     * Functions that check every 500 milliseconds if the user activated the NFC
+     */
+    private fun checkNfcBack(nfcAdapter: NfcAdapter, buttons: Array<Button>) {
+        if (nfcAdapter.isEnabled) {
+            buttons.forEach { it.visibility = View.VISIBLE }
+        }
+        Handler().postDelayed({ checkNfcBack(nfcAdapter, buttons) }, 5 * 100)
     }
 }
